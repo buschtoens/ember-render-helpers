a217d3386bec466fbcb0342f24a16e7728b8bbba
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-05-04 15:17:14 +0200

    chore: Replaced ember-try with @embroider/try

---
+++ test-app/.try.mjs
@@ -0,0 +1,53 @@
+export default {
+  packageManager: 'pnpm',
+  scenarios: [
+    {
+      name: 'ember-lts-4.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~4.12.0',
+        },
+      },
+    },
+    {
+      name: 'ember-lts-5.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~5.12.0',
+        },
+      },
+    },
+    {
+      name: 'ember-lts-6.12',
+      npm: {
+        devDependencies: {
+          'ember-source': '~6.12.0',
+        },
+      },
+    },
+    {
+      name: 'ember-release',
+      npm: {
+        devDependencies: {
+          'ember-source': 'npm:ember-source@latest',
+        },
+      },
+    },
+    // {
+    //   name: 'ember-beta',
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': 'npm:ember-source@beta',
+    //     },
+    //   },
+    // },
+    // {
+    //   name: 'ember-canary',
+    //   npm: {
+    //     devDependencies: {
+    //       'ember-source': 'npm:ember-source@alpha',
+    //     },
+    //   },
+    // },
+  ],
+};
